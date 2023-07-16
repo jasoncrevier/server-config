c61427bda39731fe99f6a38ea006087de7d870eb
Jason Crevier <jason@jasoncrevier.com>
2023-07-16 13:03:27 -0400

    Added WIP debian setup script

---
+++ basic server setup/debian_setup_WIP.sh
@@ -0,0 +1,23 @@
+# Prompt the user to enter the username that should get sudo access
+echo What username do you want to give sudo access to?
+read VAR_USER
+
+# Update and install additional packages
+apt update -y
+apt install ssh sudo ca-certificates curl gnupg micro fish -y
+
+# Give the username from before sudo access
+usermod -aG sudo $VAR_USER
+
+# Add the docker repository (these steps are from https://docs.docker.com/engine/install/debian)
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update and install docker packages
+apt update -y
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
